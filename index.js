c4a758a8d15724d4d03911ef946bc7a7a7e6a906
dhens <dawsonhensel@gmail.com>
2020-02-09 17:20:36 -0800

    added all questions we will ask the user, added inquirer and its dependencies into the .gitignore

---
+++ index.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        type: input,
+        message: 'What is your favorite color',
+        name: userColor
+    },
+    {
+        type: input,
+        message: 'Enter username to search',
+        name: userToSearch
+    }
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {}
+
+init();
